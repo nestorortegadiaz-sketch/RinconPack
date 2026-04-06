41d6534ce1c371747548bf923e495a22ba0219b0
Nestor Ortega <Nestorortegadiaz@alumno.ieselrincon.com>
2026-04-06 10:35:27 +0100

    Commit inicial

---
+++ src/Almacen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.HashSet;
+/**
+ *
+ * @author Nestor
+ */
+public class Almacen {
+    //Array Bidimensional de objetos Paquete de3 3x3
+    public void muelleFisico(){
+        Paquete[][] muelle = new Paquete [3][3];
+    }      
+     HashSet<String> listadoPaquete = new HashSet<>();
+}
