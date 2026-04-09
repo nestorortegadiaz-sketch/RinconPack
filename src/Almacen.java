a9a40ffaa79fbfbc9d0ee1a28768f0fdd1d3a054
Nestor Ortega <Nestorortegadiaz@alumno.ieselrincon.com>
2026-04-09 10:04:03 +0100

    Implementacion basica de la clase Almacen

--- src/Almacen.java
+++ src/Almacen.java
@@ -3,14 +3,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * @author Nestor
  */
 public class Almacen {
-    //Array Bidimensional de objetos Paquete de3 3x3
-    public void muelleFisico(){
-        Paquete[][] muelle = new Paquete [3][3];
-    }      
-     HashSet<String> listadoPaquete = new HashSet<>();
+
+        //Array Bidimensional de objetos Paquete de3 3x3
+        Paquete[][] muelle = new Paquete[3][3];
+        //HashSet<String> para almacenar los códigos de los paquetes
+        HashSet<String> listadoPaquete = new HashSet<>();
+        //Un ArrayList<Paquete> para los paquetes que han sido despachados
+        ArrayList<Paquete> despachados = new ArrayList<>();
+        //HashMap que asocie cada ciudad de destino.
+        HashMap<String, Integer> estadisticas = new HashMap<>();
+    
+    public void recepcionarPaquete(Paquete p, int fila, int col){
+    }
+    public void enviarACamion(int fila, int col){  
+    }
+    public void mantenimientoSeguridad(double pesoMaximo){
+    }
 }
--- src/Paquete.java
+++ src/Paquete.java
@@ -1,12 +1,53 @@
+
+import java.util.Objects;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Nestor
  */
 public class Paquete {
-    
+
+    private String codigo;
+    private double peso;
+    private String destino;
+
+    public Paquete(String codigo, double peso, String destino) {
+        this.codigo = codigo;
+        this.peso = peso;
+        this.destino = destino;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Paquete other = (Paquete) obj;
+        return Objects.equals(this.codigo, other.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.codigo);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Paquete{" + "codigo=" + codigo + ", peso=" + peso + ", destino=" + destino + '}';
+    }
+
+   
+
 }
